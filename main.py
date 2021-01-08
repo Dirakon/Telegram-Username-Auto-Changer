4c286bc89a16bffd1d32c257f083b92f31fb9a37
Dirakon <609700zxc@mail.ru>
2021-01-08 11:26:47 +0000

    A bit improved version, bigger alphabet, can stand "a wait of x seconds is required" error.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import asyncio
 import os
 import time
 from random import randrange
+from telethon import errors
 
 api_id = os.getenv('app_id')
 api_hash = os.getenv('app_hash')
@@ -15,22 +16,31 @@ api_hash = os.getenv('app_hash')
 keep_alive()
 
 ses_string = os.getenv('ses_string')
-alph = ['a','b','c','d','e','f','g','h','i','g','k','A','B','D','C','E','G','F']
+alph = ['a','b','c','d','e','f','g','h','i','g','k','A','B','D','C','E','G','F','x','X','v','V','w','W','l','L','m','M','n','N','R','r','q','Q','p','P','j','J','s','S','z','Z']
 with TelegramClient(StringSession(ses_string), api_id, api_hash) as client:
     while True:
-      time.sleep(2500)
-      print("woken")
-      usernamer = ""
-      for i in range(0,randrange(32-7)+7):
-          usernamer+=alph[randrange(alph.__len__())]
-      print(usernamer)
-      result = client(functions.account.CheckUsernameRequest(
-          username=usernamer
-      ))
-      print(result)
-      if not result:
-        continue
-      result = client(functions.account.UpdateUsernameRequest(
-          username=usernamer
-      ))
-      print(result)
+        try:
+            time.sleep(2500)
+            #print("woken")
+            usernamer = ""
+            for i in range(0,randrange(32-7)+7):
+                usernamer+=alph[randrange(alph.__len__())]
+            #print(usernamer)
+            result = client(functions.account.CheckUsernameRequest(
+                username=usernamer
+            ))
+            print(result)
+            if not result:
+              continue
+            result = client(functions.account.UpdateUsernameRequest(
+                username=usernamer
+            ))
+        except errors.FloodWaitError as e:
+            print('Have to sleep', e.seconds, 'seconds')
+            time.sleep(e.seconds+10)
+      #print(result)
+
+
+
+
+
