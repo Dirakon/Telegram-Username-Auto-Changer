9408e1596d6c799346ae87f4ee96b00240a85300
Dirakon <609700zxc@mail.ru>
2021-01-16 20:23:47 +0000

    Made the code a bit more readable and commented some things.

--- main.py
+++ main.py
@@ -11,34 +11,48 @@ from telethon import errors
 api_id = os.getenv('app_id')
 api_hash = os.getenv('app_hash')
 
+# Use this (retrieve sessionString in the runtime):
 
+with TelegramClient(StringSession(), api_id, api_hash) as client:
+
+    ses_string = client.session.save()
+    
+# Or this (keep it saved in .env):
+#ses_string = os.getenv('ses_string')
+    
 
 keep_alive()
 
-ses_string = os.getenv('ses_string')
 alph = ['a','b','c','d','e','f','g','h','i','g','k','A','B','D','C','E','G','F','x','X','v','V','w','W','l','L','m','M','n','N','R','r','q','Q','p','P','j','J','s','S','z','Z']
 with TelegramClient(StringSession(ses_string), api_id, api_hash) as client:
     while True:
         try:
-            time.sleep(2500)
-            #print("woken")
+            # Waits 2500 seconds (~40 minutes) each iteration
+            time.sleep(10)
+            
+
+
             usernamer = ""
             for i in range(0,randrange(32-7)+7):
                 usernamer+=alph[randrange(alph.__len__())]
-            #print(usernamer)
+              
             result = client(functions.account.CheckUsernameRequest(
                 username=usernamer
             ))
+
+            # Prints True if the randomly generated username is available
             print(result)
+
+
             if not result:
               continue
             result = client(functions.account.UpdateUsernameRequest(
                 username=usernamer
             ))
         except errors.FloodWaitError as e:
-            print('Have to sleep', e.seconds, 'seconds')
+            print('Have to sleep ' + str(e.seconds) + ' seconds')
             time.sleep(e.seconds+10)
-      #print(result)
+            
 
 
 
